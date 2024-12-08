9fc25eb6ec2758235c2a5faf0e7d36fbf9f32429
Rob <robin.besson1@gmail.com>
2024-12-08 21:27:56 +0100

    create pipe function and fix compose

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,9 @@
 const compose = (...funcs: Function[]) => (initialValue: any) => {
+  return funcs.reduceRight((acc, func) => func(acc), initialValue);
+};
+
+const pipe = (...funcs: Function[]) => (initialValue: any) => {
   return funcs.reduce((acc, func) => func(acc), initialValue);
 };
 
-export { compose };
+export { compose, pipe };
--- test/helpers/index.test.ts
+++ test/helpers/index.test.ts
@@ -1,10 +1,17 @@
-import { compose } from '../../src/helpers/index';
+import { compose, pipe } from '../../src/helpers/index';
 
 describe('Helpers', () => {
   test('Compose function - with mathematical operations', () => {
     const add = (a: number) => a + 10;
     const multiply = (b: number) => b * 2;
     const operation = compose(add, multiply);
+    expect(operation(2)).toBe(14);
+  });
+  
+  test('Pipe function - with mathematical operations', () => {
+    const add = (a: number) => a + 10;
+    const multiply = (b: number) => b * 2;
+    const operation = pipe(add, multiply);
     expect(operation(2)).toBe(24);
   });
 });

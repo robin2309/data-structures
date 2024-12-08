f7c43ecac44fb5d562c7a3ffde20ce1f08a41f8f
Rob <robin.besson1@gmail.com>
2024-12-08 23:47:52 +0100

    custom array filter function

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,4 +6,16 @@ const pipe = (...funcs: Function[]) => (initialValue: any) => {
   return funcs.reduce((acc, func) => func(acc), initialValue);
 };
 
-export { compose, pipe };
+const filterArray = (array: Array<any>, filterFunc: Function) => {
+  let i = 0;
+  while(i < array.length) {
+    if (!filterFunc(array[i])) {
+      array.splice(i, 1);
+    } else {
+      i++;
+    }
+  }
+  return array;
+} 
+
+export { compose, pipe, filterArray };
--- test/helpers/index.test.ts
+++ test/helpers/index.test.ts
@@ -1,4 +1,4 @@
-import { compose, pipe } from '../../src/helpers/index';
+import { compose, pipe, filterArray } from '../../src/helpers/index';
 
 describe('Helpers', () => {
   test('Compose function - with mathematical operations', () => {
@@ -13,5 +13,16 @@ describe('Helpers', () => {
     const multiply = (b: number) => b * 2;
     const operation = pipe(add, multiply);
     expect(operation(2)).toBe(24);
+  });
+  
+  test('Filter function - filters number > 4', () => {
+    const array = [1, 2, 3, 4, 5, 6];
+    const filterFunc = (elt: number) => elt <= 4;
+    expect(filterArray(array, filterFunc)).toEqual([1, 2, 3, 4]);
+  });
+  test('Filter function - filters number > 4, no element deletion', () => {
+    const array = [1, 2, 3, 4];
+    const filterFunc = (elt: number) => elt <= 4;
+    expect(filterArray(array, filterFunc)).toEqual(array);
   });
 });
